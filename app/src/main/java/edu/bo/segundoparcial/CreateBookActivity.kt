80a46062958e0759a2ac570ff1c106f439957970
Juan Jose <juanjo1998_x@hotmail.com>
2020-10-07 10:41:00 -0400

    Añadir un nuevo libro a la base de datos desde la activity CreateBook

--- app/src/main/java/edu/bo/segundoparcial/CreateBookActivity.kt
+++ app/src/main/java/edu/bo/segundoparcial/CreateBookActivity.kt
@@ -2,10 +2,53 @@ package edu.bo.segundoparcial
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.EditText
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_create_book.*
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class CreateBookActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_book)
+
+
+        button_create.setOnClickListener {
+            GlobalScope.launch {
+                val bookDao = AppRoomDatabase.getDatabase(applicationContext).bookDato()
+                val repository = BookRepository(bookDao)
+
+                val editTextTitle = findViewById<EditText>(R.id.editTextTextTitle)
+                val editTextTitleValue = editTextTitle.text
+                val editTextPages = findViewById<EditText>(R.id.editTextTextPages)
+                val editTextPagesValue = editTextPages.text
+                val editTextEditorial = findViewById<EditText>(R.id.editTextTextEditorial)
+                val editTextEditorialValue = editTextEditorial.text
+                val editTextTextAuthor = findViewById<EditText>(R.id.editTextTextAuthor)
+                val editTextTextAuthorValue = editTextTextAuthor.text
+                val editTextTextDescription = findViewById<EditText>(R.id.editTextTextDescription)
+                val editTextTextDescriptionValue = editTextTextDescription.text
+                val editTextTextPhotoUrl = findViewById<EditText>(R.id.editTextTextPhotoUrl)
+                val editTextTextPhotoUrlValue = editTextTextPhotoUrl.text
+
+//                Log.d("Prueba","title = ${editTextValue}")
+
+                repository.insert(
+                    Book(
+                        editTextTitleValue.toString(),
+                        editTextPagesValue.toString().toInt(),
+                        editTextEditorialValue.toString(),
+                        editTextTextAuthorValue.toString(),
+                        editTextTextDescriptionValue.toString(),
+                        editTextTextPhotoUrlValue.toString()
+                    )
+                )
+
+            }
+
+        }
     }
 }
--- app/src/main/java/edu/bo/segundoparcial/MainActivity.kt
+++ app/src/main/java/edu/bo/segundoparcial/MainActivity.kt
@@ -23,13 +23,8 @@ class MainActivity : AppCompatActivity() {
             val bookDao = AppRoomDatabase.getDatabase(applicationContext).bookDato()
             val repository = BookRepository(bookDao)
 
-//            button_create.setOnClickListener{
-//
-//                repository.insert(Book(editTextTextTitle.toString(),editTextTextPages.toString().toInt(),editTextTextEditorial.toString(),editTextTextAuthor.toString(),editTextTextDescription.toString(),editTextTextPhotoUrl.toString()))
-//            }
 
-
-            repository.insert(Book("the best seller: Android",22,"Editorial1","JuanJose","descripcion del libro","https://i.pinimg.com/474x/a8/6e/26/a86e26dffbcd0f8ffd0b7a6a4809ec68.jpg"))
+//            repository.insert(Book("the best seller: Android",22,"Editorial1","JuanJose","descripcion del libro","https://i.pinimg.com/474x/a8/6e/26/a86e26dffbcd0f8ffd0b7a6a4809ec68.jpg"))
             val lista = repository.getListBooks()
             lista.forEach {
                 Log.d("DBTEST","Id book = ${it.id}, Title: ${it.title}, Pages: ${it.pages}, Editorial: ${it.editorial}, Author: ${it.author}, Descripcion: ${it.description}, Url: ${it.photoUrl}")
@@ -41,28 +36,6 @@ class MainActivity : AppCompatActivity() {
             linearLayoutManager.orientation = LinearLayoutManager.VERTICAL
             recyclerView.layoutManager = linearLayoutManager
 
-//            val lista1 = arrayListOf<Book>()
-//            lista.forEach {
-//                lista1.add(Book("${it.title}","${it.pages}".toInt(),"${it.editorial}","${it.author}","${it.description}","${it.photoUrl}"))
-//                val bookListAdapter = BookListAdapter(lista1, this@MainActivity)
-//                recyclerView.adapter = bookListAdapter
-//                val linearLayoutManager = LinearLayoutManager(this@MainActivity)
-//                linearLayoutManager.orientation = LinearLayoutManager.VERTICAL
-//                recyclerView.layoutManager = linearLayoutManager
-//
-////                Log.d("DBTEST","Id book = ${it.id}, Title: ${it.title}, Pages: ${it.pages}, Editorial: ${it.editorial}, Author: ${it.author}, Descripcion: ${it.description}, Url: ${it.photoUrl}")
-//            }
-
-
-//            val lista1 = arrayListOf<Book>()
-//            lista1.add(Book("the best seller: Android",50,"Editorial1","JuanJose","descripcion del libro","url"))
-//            lista1.add(Book("the best seller: Android 2",22,"Editorial2","Pepe","descripcion del libro 2","url 2"))
-//
-//            val bookListAdapter = BookListAdapter(lista1, this@MainActivity)
-//            recyclerView.adapter = bookListAdapter
-//            val linearLayoutManager = LinearLayoutManager(this@MainActivity)
-//            linearLayoutManager.orientation = LinearLayoutManager.VERTICAL
-//            recyclerView.layoutManager = linearLayoutManager
         }
 
 
